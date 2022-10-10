12edc7552af591f6ae1383351fa0653b210e322b
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-10-10 00:23:46 -0300

    Update PermutarListas.py

--- PermutarListas.py
+++ PermutarListas.py
@@ -1,16 +1,11 @@
-def permutar(a=None,permut=list(range(3)),lista=list()):
+def permutar(permut=list(range(3))):
     if len(permut) > 1:
         l=list()
         for i in permut:
             temp=[j for j in permut]
-            t=[j for j in lista]
             temp.remove(i)
-            t.append(i)
-            for p in permutar(a,temp,t): 
+            for p in permutar(temp): 
                 l.append([i]+p)
         return l
     else:
-        if a != None:
-            lista.append(permut[0])
-            a.append(lista)
         return [permut]
